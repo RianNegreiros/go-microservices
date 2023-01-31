33aa0487062ecf4bd2a67bcfb3f78ffdb3674f04
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-01-31 17:59:21 -0300

    handle submission

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -7,14 +7,15 @@ import (
 	"net/http"
 )
 
-type JsonResponse struct {
+type jsonResponse struct {
 	Error   bool   `json:"error"`
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
 }
 
+// readJSON tries to read the body of a request and converts it into JSON
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
-	maxBytes := 1048576
+	maxBytes := 1048576 // one megabyte
 
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
@@ -26,21 +27,22 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
-		return errors.New("request body must only contain a single JSON object")
+		return errors.New("body must have only a single JSON value")
 	}
 
 	return nil
 }
 
-func (app *Config) writeJSON(w http.ResponseWriter, r *http.Request, status int, data any, headers ...http.Header) error {
+// writeJSON takes a response status code and arbitrary data and writes a json response to the client
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
 	out, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
 	if len(headers) > 0 {
-		for k, v := range headers[0] {
-			w.Header()[k] = v
+		for key, value := range headers[0] {
+			w.Header()[key] = value
 		}
 	}
 
@@ -54,16 +56,18 @@ func (app *Config) writeJSON(w http.ResponseWriter, r *http.Request, status int,
 	return nil
 }
 
-func (app *Config) errorJSON(w http.ResponseWriter, r *http.Request, err error, status ...int) error {
+// errorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
 	if len(status) > 0 {
 		statusCode = status[0]
 	}
 
-	var payload JsonResponse
+	var payload jsonResponse
 	payload.Error = true
 	payload.Message = err.Error()
 
-	return app.writeJSON(w, r, statusCode, payload)
+	return app.writeJSON(w, statusCode, payload)
 }
