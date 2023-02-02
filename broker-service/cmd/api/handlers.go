4464ddcf041d97861ae8ab5f40ab00d5275b62f6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-02-02 17:58:41 -0300

    add rpc to broker

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -5,8 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-
-	"github.com/RianNegreiros/go-microservices/broker-service/cmd/event"
+	"net/rpc"
 )
 
 type RequestPayload struct {
@@ -57,7 +56,7 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
 	case "log":
-		app.logEventViaRabbit(w, requestPayload.Log)
+		app.logItemViaRPC(w, requestPayload.Log)
 	case "mail":
 		app.sendMail(w, requestPayload.Mail)
 	default:
@@ -155,36 +154,68 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 
 }
 
-func (app *Config) logEventViaRabbit(w http.ResponseWriter, l LogPayload) {
-	err := app.pushToQueue(l.Name, l.Data)
+// func (app *Config) logEventViaRabbit(w http.ResponseWriter, l LogPayload) {
+// 	err := app.pushToQueue(l.Name, l.Data)
+// 	if err != nil {
+// 		app.errorJSON(w, err)
+// 		return
+// 	}
+
+// 	var payload jsonResponse
+// 	payload.Error = false
+// 	payload.Message = "logged"
+
+// 	app.writeJSON(w, http.StatusAccepted, payload)
+// }
+
+// func (app *Config) pushToQueue(name, msg string) error {
+// 	emitter, err := event.NewEventEmitter(app.Rabbit)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	payload := LogPayload{
+// 		Name: name,
+// 		Data: msg,
+// 	}
+
+// 	j, _ := json.MarshalIndent(payload, "", "\t")
+// 	err = emitter.Push(string(j), "log.INFO")
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+type RPCPayload struct {
+	Name string
+	Data string
+}
+
+func (app *Config) logItemViaRPC(w http.ResponseWriter, l LogPayload) {
+	client, err := rpc.Dial("tcp", "logger-service:5001")
 	if err != nil {
 		app.errorJSON(w, err)
 		return
 	}
 
-	var payload jsonResponse
-	payload.Error = false
-	payload.Message = "logged"
+	rpcPayload := RPCPayload{
+		Name: l.Name,
+		Data: l.Data,
+	}
+
+	var result string
+	err = client.Call("RPCServer.LogInfo", rpcPayload, &result)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: result,
+	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
-}
-
-func (app *Config) pushToQueue(name, msg string) error {
-	emitter, err := event.NewEventEmitter(app.Rabbit)
-	if err != nil {
-		return err
-	}
-
-	payload := LogPayload{
-		Name: name,
-		Data: msg,
-	}
-
-	j, _ := json.MarshalIndent(payload, "", "\t")
-	err = emitter.Push(string(j), "log.INFO")
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
