0db29134c6bab6f3a18d518df572913ffd589207
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-02-02 15:41:19 -0300

    connect to rabbitmq

---
+++ listener-service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ")
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+		if err != nil {
+			fmt.Println("Trying to connect to RabbitMQ...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Reconnecting in ", backOff)
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
+}
