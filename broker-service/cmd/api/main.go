48497db1a50fa81516f767199e7f51ab6800a72a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-02-02 16:56:40 -0300

    add listener to brokr

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const webPort = "80"
 
-type Config struct{}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	app := Config{
+		Rabbit: rabbitConn,
+	}
 
 	log.Printf("Starting broker servicer on port %s\n", webPort)
 
@@ -20,8 +36,37 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("Trying to connect to RabbitMQ...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Reconnecting in ", backOff)
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
