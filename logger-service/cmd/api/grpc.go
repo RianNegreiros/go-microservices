b8dd145a838978f62a0a9f9d30e300c5c6ec3161
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-02-02 19:23:59 -0300

    add grpc

--- logger-service/cmd/api/grpc.go
+++ logger-service/cmd/api/grpc.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
 
 	"github.com/RianNegreiros/go-microservices/logger-service/data"
 	"github.com/RianNegreiros/go-microservices/logger-service/logs"
+	"google.golang.org/grpc"
 )
 
 type LogServer struct {
@@ -28,4 +32,21 @@ func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.L
 
 	res := &logs.LogResponse{Result: "logged"}
 	return res, nil
+}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRpcPort))
+	if err != nil {
+		log.Fatalf("Failed to listen for gRPC: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	logs.RegisterLogServiceServer(s, &LogServer{Models: app.Models})
+
+	log.Printf("gRPC listening on port %s", gRpcPort)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC: %v", err)
+	}
 }
